27f3ab5e28101b9f7645b22f477da946b617851d
rdarshana <rukshan.asela87@gmail.com>
2016-11-24 06:59:36 +0530

    added bootstrap js reference

--- Source/application/views/welcome_message.php
+++ Source/application/views/welcome_message.php
@@ -99,5 +99,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
+    <script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-3.1.1.js"); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
 </body>
 </html>
